bb7e1815bde58012d80082d7bed82ea5b3e51b26
danish <danishalbela8@gmail.com>
2023-11-08 00:00:41 +0530

    created header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  mobileNumber: string = '123-456-7890';
 
+  sendEmail() {
+    window.location.href = 'mailto:your@email.com'; // Replace with your email address
+  }
+
+  copyMobile() {
+    const textArea = document.createElement('textarea');
+    textArea.value = this.mobileNumber;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+  }
 }
