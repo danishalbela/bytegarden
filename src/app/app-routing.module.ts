3a22c332575615e2f64b918ea0f38503a28db168
Atul <aatulgyan444@gmail.com>
2023-11-12 07:53:27 +0530

    using bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 
 const routes: Routes = [
-  {path:'home',component:HomeComponent},
+  {path:'',component:HomeComponent},
  {path:'about',component:AboutComponent}
 ];
 
